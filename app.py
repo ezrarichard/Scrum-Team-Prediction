21142d035da8f4a8d7c3b5a8295b463b14cb2d85
Ezra Richard <125721936+ezrarichard@users.noreply.github.com>
2024-03-03 15:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ model = train_and_evaluate_model()
 
 # User inputs
 st.header('Enter Sprint Details:')
-days_since_first_sprint = st.number_input('Days Since First Sprint', min_value=0)
+
+team = st.number_input('Team Size', min_value=1, max_value=20)  # Assuming team size ranges from 1 to 20
 leave = st.number_input('Leave', min_value=0)
 working_days = st.number_input('Working Days', min_value=1)
 availability = st.number_input('Availability', min_value=0.0, max_value=10.0, step=0.5)
 
 if st.button('Predict Story Points and Completion'):
-    story_points, story_completed = predict_story_points(model, days_since_first_sprint, leave, working_days, availability)
+    story_points, story_completed = predict_story_points(model, team, leave, working_days, availability)
     st.success(f'Predicted Story Points: {story_points}, Predicted Story Completion: {story_completed}')
